448ff1237662651ee07f15de9d65fcab4e8e758a
Juulvt <juulvantilburg@gmail.com>
2023-10-30 13:37:25 +0100

    remove unnecessary

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
-            $table->foreignId('role_id')->constrained()->nullable()->change();;
+            $table->foreignId('role_id')->constrained()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
